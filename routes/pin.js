3e2487f920c344c8a7c9323911a76da5ad67193d
sravanikodipelly1 <S544914@NWMISSOURI.EDU>
2021-12-01 00:20:01 -0600

    secured update

--- routes/pin.js
+++ routes/pin.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const pin_controllers= require('../controllers/pin');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 /* GET pins */
 router.get('/', pin_controllers.pin_view_all_Page );
 /* GET detail pin page */
@@ -8,7 +18,7 @@ router.get('/detail', pin_controllers.pin_view_one_Page);
 /* GET create pin page */
 router.get('/create', pin_controllers.pin_create_Page);
 /* GET create update page */
-router.get('/update', pin_controllers.pin_update_Page);
+router.get('/update', secured, pin_controllers.pin_update_Page);
 /* GET create pin page */
 router.get('/delete', pin_controllers.pin_delete_Page);
 
